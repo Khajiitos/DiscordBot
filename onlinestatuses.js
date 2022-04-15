8a6445afe331f48fc6c4106349a9a2766792a27e
Khajiitos <khajiit112@gmail.com>
2022-04-15 22:18:10 +0200

    Recode onlinestatuses and make it use binary files

--- onlinestatuses.js
+++ onlinestatuses.js
@@ -2,96 +2,74 @@ const Discord = require('discord.js');
 const client = require('./main');
 const fs = require('fs');
 
-class UserWithStatus {
-    userNameAndTag = "ab#1234";
-    userid = 0;
-    timestamp = 0;
-    status = 'f';
+let timeout = null;
+
+function msToNextFullMinute() {
+    const date = new Date();
+    return 60000 - ((date.getSeconds() * 1000) + date.getMilliseconds());
 }
 
-function isUsingPhone(clientStatus) {
-    if (clientStatus == null)
-        return false;
+function update() {
+    updateOnlineStatuses();
+    timeout = setTimeout(update, msToNextFullMinute());
+}
 
-    let desktop = clientStatus.desktop;
-    let web = clientStatus.web;
-    let mobile = clientStatus.mobile;
+function presenceToChar(presence) {
 
-    if ( (web && web === 'idle') || (desktop && desktop === 'idle') && mobile)
-        return true;
+    if (!presence) return 'f';
 
-    return !desktop && !web && mobile;
+    let phone = false;
+
+    if (presence.clientStatus) {
+        const desktop = presence.clientStatus.web;
+        const web = presence.clientStatus.web;
+        const mobile = presence.clientStatus.mobile;
+
+        phone = (!desktop || desktop === 'idle' || desktop == 'offline') && 
+        (!web || web === 'idle' || web == 'offline') && mobile;
+    }
+    
+    switch(presence.status) {
+        case 'offline': return phone ? 'F' : 'f';
+        case 'online': return phone ? 'O' : 'o';
+        case 'dnd': return phone ? 'D' : 'd';
+        case 'idle': return phone ? 'I' : 'i';
+        default: return 'f';
+    }
 }
 
 function updateOnlineStatuses() {
-    //online, offline, dnd, idle
-    let arr = [];
-    let count = 0;
-
-    function cb(arra) {
-
-        let nameDictionaryString = "";
-        arra.forEach(val => {
-
-            let filepath = 'statusdata/';
-            let writethis = val.timestamp + val.status + '\n';
-
-            if (!fs.existsSync(filepath))
-                fs.mkdirSync(filepath);
-
-            filepath += val.userid + '.txt';
-            fs.appendFile(filepath, writethis, function (err) {
-                if (err) return console.log(err);
-            });
-            nameDictionaryString += val.userNameAndTag + ':' + val.userid + '\n';
-        });
-        fs.writeFile('namedictionary.txt', nameDictionaryString, function (err) {
-            if (err) return console.log(err);
-        });
-    }
-
-    function shortenedOnlineStatus(status, phone = false) {
-        switch(status) {
-            case "offline": return phone ? 'F' : 'f';
-            case "online": return phone ? 'O' : 'o';
-            case "dnd": return phone ? 'D' : 'd';
-            case "idle": return phone ? 'I' : 'i';
-        }
-        return '!';
-    }
-
-    function secondsSince2021start() {
-        return Math.floor((Date.now() / 1000)) - 1609459200;
-    }
-
+    let users = [];
     client.guilds.cache.forEach(guild => {
         guild.members.cache.forEach(guildMember => {
-            let user = new UserWithStatus();
-            user.userNameAndTag = guildMember.user.username + '#' + guildMember.user.discriminator;
-            user.timestamp = secondsSince2021start();
-            user.userid = guildMember.user.id;
-            user.status = shortenedOnlineStatus('offline', false); // fix this later
-            arr.push(user); 
+            if (!users.some(obj => obj.id === guildMember.user.id)) {
+                users.push({
+                    name: guildMember.user.username + '#' + guildMember.user.discriminator,
+                    id: guildMember.user.id,
+                    presenceChar: presenceToChar(guildMember.presence)
+                });
+            }
         });
     });
+    
+    if (!fs.existsSync('statusdata/')){
+        fs.mkdirSync('statusdata/');
+    }
 
-    let uniqueArray = [...new Map(arr.map(item => [item['userid'], item])).values()]
-    cb(uniqueArray);
+    let nameDictionaryString = '';
+
+    users.forEach(user => {
+        let buffer = new Buffer.allocUnsafe(5);
+        buffer.writeUInt32BE(Math.floor(Date.now() / 1000));
+        buffer.write(user.presenceChar, 4);
+        console.log(buffer);
+        fs.appendFile(`statusdata/${user.id}`, buffer, ()=>{});
+        nameDictionaryString += user.name + ':' + user.id + '\n';
+    });
+
+    fs.writeFile('namedictionary.txt', nameDictionaryString, ()=>{});
 }
 
 client.once('ready', () => {
-
-    function msToNextFullMinute() {
-        let date = new Date();
-        let ret =  60000 - ((date.getSeconds() * 1000) + date.getMilliseconds());
-        return ret;
-    }
-
-    function update() {
-        updateOnlineStatuses();
-        clearInterval(timer);
-        timer = setInterval(update, msToNextFullMinute());
-    }
-
-    var timer = setInterval(update, msToNextFullMinute());
+    timeout = setTimeout(update, msToNextFullMinute());
 });
